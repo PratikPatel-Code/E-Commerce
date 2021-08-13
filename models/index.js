c4dc80566c061719ddbbe118bfd475d31638e787
Pratik Patel <pratikpatel_85@yahoo.com>
2021-08-13 19:09:28 -0400

    Fixed my routes and index, final push

--- models/index.js
+++ models/index.js
@@ -1,35 +1,35 @@
 // standard imports
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-    foreignKey: 'category_id',
-    onDelete: 'CASCADE',
+  foreignKey: "category_id",
 });
 // Categories have many Products
 Category.hasMany(Product, {
-    foreignKey: 'category_id',
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-    through: {
-        model: ProductTag,
-        unique: false
-    }
+  through: {
+    model: "product_tag",
+    unique: false,
+  },
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-    through: {
-        model: ProductTag,
-        unique: false
-    }
-})
+  through: {
+    model: "product_tag",
+    unique: false,
+  },
+});
 module.exports = {
-    Product,
-    Category,
-    Tag,
-    ProductTag,
+  Product,
+  Category,
+  Tag,
+  ProductTag,
 };
